7f21cb80df58810daf15ca973e78c6c98ca857ed
bbkilla <50890686+bbkilla@users.noreply.github.com>
2025-04-01 18:50:23 +0300

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -4,5 +4,6 @@ import requests
 API = "https://api.torn.com/torn/?key=PHjfnNpLPwYwlM7l&comment=TornAPI&selections=shoplifting"
 DATA_FILE = "status_history.json"
 response = requests.get(API)
+data = response.json()
 with open(DATA_FILE, "w") as f:
     json.dump(data, f, indent=4)
