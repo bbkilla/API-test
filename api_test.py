40c733297e8b807e2f2e13688b88487b0c46b456
bbkilla <50890686+bbkilla@users.noreply.github.com>
2025-04-01 18:25:47 +0300

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,6 +1,7 @@
-import http.client
-
-conn = http.client.HTTPSConnection("api.github.com")
-conn.request("GET", "/")
-res = conn.getresponse()
-print(f"Status: {res.status}, Response: {res.read().decode()}")
+import os
+import json
+def load_history():
+    if not os.path.exists(DATA_FILE):  # אם הקובץ לא קיים, יוצרים אותו עם תוכן ריק
+        with open(DATA_FILE, "w") as f:
+            json.dump({}, f, indent=4)  # כותבים מילון ריק כדי למנוע שגיאות
+        return {}
