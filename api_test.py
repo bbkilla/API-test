3eef076ff24a07f1edcc21fbd7e90f5d39685788
bbkilla <50890686+bbkilla@users.noreply.github.com>
2025-04-02 10:47:53 +0300

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from datetime import datetime
 
 API = "https://api.torn.com/torn/?key=PHjfnNpLPwYwlM7l&comment=TornAPI&selections=shoplifting"
 DATA_FILE = "status_history.json"
@@ -8,3 +9,7 @@ data = response.json()
 with open(DATA_FILE, "w") as f:
     json.dump(data, f, indent=4)
 print(data)
+timestamp = datetime.utcnow().isoformat()
+print(timestamp)
+with open(DATA_FILE, "a") as f:
+    json.dump(timestamp, f, indent=4)
